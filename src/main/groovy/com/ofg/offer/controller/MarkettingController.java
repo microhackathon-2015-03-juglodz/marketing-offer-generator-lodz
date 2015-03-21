492870eaa3ab7767b32d99c3da06d08360ba11d1
Piotr Zawadzki <pzawadzki82@gmail.com>
2015-03-21 14:30:10 +0100

    MarkettingOffer - getOffer

--- src/main/groovy/com/ofg/offer/controller/MarkettingController.java
+++ src/main/groovy/com/ofg/offer/controller/MarkettingController.java
@@ -1,6 +1,8 @@
 package com.ofg.offer.controller;
 
 
+import java.util.concurrent.Callable;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +12,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ofg.offer.dto.LoanDecision;
+import com.ofg.offer.dto.MarketingOffer;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api/marketting")
-@Api(value = "loanApplicationId", description = "Collects places from tweets and propagates them to Collerators")
+@Api(value = "MarkettingOfferGenerator", description = "Generates offers")
 public class MarkettingController {
 	
 	@RequestMapping(
@@ -27,5 +30,20 @@ public class MarkettingController {
             notes = "This will asynchronously call tweet collecting, place extracting and their propagation to Collerators")
 	public void generateMarketingOffer(@PathVariable long loanApplicationId, LoanDecision loanDecision){
 		System.out.println("Works!!!!!!!!!!! "+loanApplicationId);
+	}
+	
+	@RequestMapping(
+            value = "{firstLastName}",
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+			)
+	@ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation(value = "Async collecting and propagating of tweets for a given pairId",
+            notes = "This will asynchronously call tweet collecting, place extracting and their propagation to Collerators")
+	public MarketingOffer getMarketingOffer(@PathVariable String firstLastName){
+		System.out.println("Works!!!!!!!!!!! "+firstLastName);
+		MarketingOffer offer = new MarketingOffer();
+		offer.setMarketingOffer("Offer");
+		return offer;
 	}
 }
